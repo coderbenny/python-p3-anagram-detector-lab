2c6e50c2aeb435f705f67f4bc4042efe251464b3
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2024-01-25 17:17:28 +0300

    completed lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,26 @@
-# your code goes here!
+# your code 
+word_list = ['enlists', 'google', 'inlets', 'banana']
+
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+
+    def word(self):
+        return self._word
+
+    def set_word(self, word):
+        self._word = word
+
+    word = property(word, set_word)
+
+    def match(self, word_list):
+        sorted_self_word = sorted(self.word)
+        result =  [item for item in word_list if sorted(item) == sorted_self_word]
+        if result:
+            return result
+        else:
+            return []
+
+listen = Anagram("listen")
+listen.match(word_list = ['enlists', 'google', 'inlets', 'banana'])
